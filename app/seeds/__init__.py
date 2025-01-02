a4bb1c8b7cf8dc9358aa12f8f4566b193f2042e4
Luis <moondad2022@gmail.com>
2025-01-01 17:15:52 -0800

    changes to db commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -7,7 +7,7 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
   if environment == "production":
-    db.session.execute(f"TRUNCATE table {SCHEMA}.doer_users RESTART IDENTITY CASCADE;")
+    db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     db.session.commit()
 
   seed_users()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,21 +1,18 @@
 from app.models import db, User
 from datetime import date
-from app.utils import avagen
 
 # Demo User
 def seed_users():
-  ava_data = avagen()
   demo = User(
     email="demo@doer.com",
     password="mypassword",
     username="demodoer",
     birthday=date(1993, 7, 11),
-    avatar=ava_data,
   )
 
   db.session.add(demo)
   db.session.commit()
 
 def undo_users():
-  db.session.execute('TRUNCATE doer_users RESTART IDENTITY CASCADE;')
+  db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
   db.session.commit()
